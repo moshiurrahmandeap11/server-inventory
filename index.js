dfd1a239bbe8e571e5eb0726a5c5d469e35c387c
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-02-08 23:48:32 +0600

    vercel deploy s

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 
 await connectDB();
-
+ 
 // Routes
 app.use("/api/users", users);
 app.use("/api/basic-settings", basicSettings);
