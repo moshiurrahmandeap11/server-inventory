e7c0dcf93d61f8d7e5e9fada8c271354b2df90d8
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-02-08 23:52:42 +0600

    vercel deploy s

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 const verifyToken = (req, res, next) => {
 
   const token = req.cookies.token;
+  console.log("token", token);
 
   if (!token) {
     return res.status(401).json({ success: false, message: "Access Denied / No Token Provided" });
