e96b412d42759eb2eebc47bdc664584b9dcdb8b0
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-02-08 23:34:31 +0600

    vercel deploy s

--- routes/usersRoute/users.js
+++ routes/usersRoute/users.js
@@ -75,8 +75,8 @@ router.post("/login", async (req, res) => {
     // send as http-only cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
